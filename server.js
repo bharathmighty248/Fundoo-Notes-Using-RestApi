c0c118c73d730a89c46a1577f9603e692c45f729
bharathmighty248 <bharathmighty248@gmail.com>
2021-12-27 17:12:41 +0530

    Feat: Configuring and Connecting to the database

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dbConfig = require('./config/database.config.js');
 
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
 
 app.get('/', (req,res) => {
     res.json({"message": "Welcome to Fundoo-Notes Application. Take notes quickly. Organize and keep track of all your notes."});
