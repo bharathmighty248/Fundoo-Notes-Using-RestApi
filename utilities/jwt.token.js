ae5da9e1f12d484596adb15d8309c66d21388c51
bharathmighty248 <bharathmighty248@gmail.com>
2021-12-28 19:41:36 +0530

    Refactor: Jwt token file refactored

--- utilities/jwt.token.js
+++ utilities/jwt.token.js
@@ -1,12 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 class GetToken {
-    getToken = (details) => {
-        const token = jwt.sign({
-            id: details._id,
-            email: details.email
-        }, process.env.JWT_SECRET)
-        return token;
+    getToken = (details, callback) => {
+        try {
+            const token = jwt.sign({
+                id: details._id,
+                email: details.email
+            }, process.env.JWT_SECRET)
+            return token;
+        } catch (error) {
+            return callback(error,null)
+        }
     }
 }
 module.exports = new GetToken();
